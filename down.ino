1440a256f1cf2f26460ca82a91938fa26ca5035c
eujoaolima <106890966+eujoaolima@users.noreply.github.com>
2022-06-04 19:28:45 -0300

    Add files via upload

---
+++ down.ino
@@ -0,0 +1,11 @@
+void down()
+{
+  digitalWrite(led4, LOW);
+  delay(75);
+  digitalWrite(led3, LOW);
+  delay(75);
+  digitalWrite(led2, LOW);
+  delay(75);
+  digitalWrite(led1, LOW);
+  delay(75);
+}
---
+++ up.ino
@@ -0,0 +1,12 @@
+void up()
+{
+  digitalWrite(led1, HIGH);
+  delay(75);
+  digitalWrite(led2, HIGH);
+  delay(75);
+  digitalWrite(led3, HIGH);
+  delay(75);
+  digitalWrite(led4, HIGH);
+  delay(75);
+  
+}
